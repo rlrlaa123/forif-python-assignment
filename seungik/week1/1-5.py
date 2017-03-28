c99fc7f2ea9baafad9098dd781006f1bb8c3128f
rlrlaa123 <kimdonghyun3366@gmail.com>
2017-03-29 08:10:22 +0900

    seungik-week1

---
+++ seungik/week1/1-5.py
@@ -0,0 +1,7 @@
+﻿#1주차 과제 5번
+
+color_list=["Red","Green","White","Black"]
+print(color_list[0],color_list[-1])
+print(color_list[-1])                   #한번에 두개 다 출력하는 법을 모르겠어요
+
+print color_list[-4:0]
